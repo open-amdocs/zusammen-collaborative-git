62cab991669f15e240849f92c70e1dd996aa56a1
Oren <orenkle@amdocs.com>
2017-05-16 16:00:06 +0300

    Check Health

---
+++ zusammen-collaboration-store-plugin/src/main/java/org/amdocs/zusammen/plugin/collaborationstore/dao/api/CollaborationHealthCheck.java
@@ -0,0 +1,6 @@
+package org.amdocs.zusammen.plugin.collaborationstore.dao.api;
+
+public interface CollaborationHealthCheck {
+    public static final String MODULE_NAME = "Collaboration";
+    public static final String HEALTH_CHECK_REPO = "HEALTH_CHECK";
+}
